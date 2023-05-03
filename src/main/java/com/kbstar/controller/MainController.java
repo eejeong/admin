9116db5f1e722025a8bb713974c87b7101480d31
eejeong <wdwaaa7325@gmail.com>
2023-05-03 15:51:26 +0900

    0503 admin

--- src/main/java/com/kbstar/controller/MainController.java
+++ src/main/java/com/kbstar/controller/MainController.java
@@ -1,54 +1,111 @@
 package com.kbstar.controller;
 
-import com.kbstar.dto.Item;
+import com.kbstar.dto.Adm;
+import com.kbstar.service.AdmService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+@Slf4j
 @Controller
 public class MainController {
+    @Autowired
+    AdmService admService;
     @RequestMapping("/")
-    public String main(){
+    public String main() {
         return "index";
     }
+    @RequestMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("center", "login");
+        return "index";
+    }
+    @RequestMapping("/logoutimpl")
+    public String logoutimpl(Model model, HttpSession session){
+        if(session != null){
+            session.invalidate();
+        }
+        return "redirect:/";
+    }
+    @RequestMapping("/loginimpl")
+    public String loginimpl(Model model, String id, String pwd, HttpSession session) throws Exception {
+        //logger.info("★★★★★★★★★★"+id+ " "+ pwd);
+        Adm adm = null;
+        String nextPage ="loginfail";
+        try {
+            adm = admService.get(id);
+            //&&가 2개면, 앞에꺼 실패하면 뒤에껀 안 함
+            if(adm != null && encoder.matches(pwd, adm.getPwd())){
+                nextPage ="loginok";
+                //session에 담은것도 jsp파일에서 $ 로 꺼낼 수 있다
+                session.setMaxInactiveInterval(100000);
+                session.setAttribute("loginadm", adm);
+            }
+        } catch (Exception e) {
+            throw new Exception("시스템장애 잠시후 다시 로그인 하세요!");
+        }
+        model.addAttribute("center", nextPage);
+        return "index";
+    }
+
+    @RequestMapping("/adminfo")
+    public String adminfo(Model model, String id) throws Exception {
+        Adm adm = null;
+        try {
+            adm = admService.get(id);
+        } catch (Exception e) {
+            throw new Exception("시스템 장애");
+        }
+        model.addAttribute("adminfo", adm);
+        model.addAttribute("center", "adminfo");
+        return "index";
+    }
+
+    @RequestMapping("/adminfoimpl")
+    public String adminfoimpl(Model model, Adm adm) throws Exception {
+        try {
+            log.info("---------------------------------------"+adm.getPwd());
+            adm.setPwd(encoder.encode(adm.getPwd()));
+            admService.modify(adm);
+        } catch (Exception e) {
+            throw new Exception("시스템 장애");
+        }
+        return "redirect:/adminfo?id="+adm.getId();
+    }
+
+    @RequestMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("center", "register");
+        return "index";
+    }
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @RequestMapping(value ="/registerimpl")
+    public String registerImpl(Model model,
+                               Adm adm, HttpSession session) throws Exception {
+        try {
+            adm.setPwd(encoder.encode(adm.getPwd()));
+            admService.register(adm);
+            session.setAttribute("loginadm", adm);
+        } catch (Exception e) {
+            throw new Exception("가입 오류");
+        }
+        model.addAttribute("radm", adm);
+        //model.addAttribute("center", "registerok");
+        return "redirect:/";
+    }
+
 
     @RequestMapping("/charts")
     public String charts(Model model) {
         model.addAttribute("center", "charts");
-        return "index";
-    }
-
-    @RequestMapping("/tables")
-    public String all(Model model){
-        List<Item> list = new ArrayList<>();
-        list.add(new Item(100, "pants1", 1000, new Date()));
-        list.add(new Item(101, "pants2", 2000, new Date()));
-        list.add(new Item(102, "pants3", 3000, new Date()));
-        list.add(new Item(103, "pants4", 4000, new Date()));
-        list.add(new Item(104, "pants5", 5000, new Date()));
-        list.add(new Item(105, "pants6", 6000, new Date()));
-        list.add(new Item(106, "pants7", 7000, new Date()));
-        list.add(new Item(107, "pants8", 8000, new Date()));
-        list.add(new Item(108, "pants9", 9000, new Date()));
-        list.add(new Item(109, "pants10", 5000, new Date()));
-        list.add(new Item(200, "skirt1", 1000, new Date()));
-        list.add(new Item(201, "skirt2", 2000, new Date()));
-        list.add(new Item(202, "skirt3", 3000, new Date()));
-        list.add(new Item(203, "skirt4", 4000, new Date()));
-        list.add(new Item(204, "skirt5", 5000, new Date()));
-        list.add(new Item(300, "shirt1", 1000, new Date()));
-        list.add(new Item(301, "shirt2", 2000, new Date()));
-        list.add(new Item(302, "shirt3", 3000, new Date()));
-        list.add(new Item(303, "shirt4", 4000, new Date()));
-        list.add(new Item(304, "shirt5", 5000, new Date()));
-
-        model.addAttribute("allitem", list);
-        model.addAttribute("left", "left");
-        model.addAttribute("center", "tables");
         return "index";
     }
 
--- src/test/java/com/kbstar/AdminApplicationTests.java
+++ src/test/java/com/kbstar/AdminApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootTest
-class Web03ApplicationTests {
+class AdminApplicationTests {
 
     @Test
     void contextLoads() {
